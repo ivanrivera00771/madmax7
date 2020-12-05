be946264d78c25e22190c6ccb910c6264f0df2c1
IvanRivera771 <47260927+IvanRivera771@users.noreply.github.com>
2020-12-05 17:59:49 -0600

    aporte para mad max 7

---
+++ src/madmax5/Madmax5.java
@@ -0,0 +1,13 @@
+
+package madmax5;
+
+
+public class Madmax5 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("madmax5");
+        System.out.println("madmax7 vamo a ver");
+    }
+    
+}
