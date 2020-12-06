3608c9c744a9d886d1a2b701f03df63b0efe883e
IvanRivera771 <47260927+IvanRivera771@users.noreply.github.com>
2020-12-05 18:02:08 -0600

    aporte para ver 2

--- src/madmax5/Madmax5.java
+++ src/madmax5/Madmax5.java
@@ -8,6 +8,7 @@ public class Madmax5 {
     public static void main(String[] args) {
         System.out.println("madmax5");
         System.out.println("madmax7 vamo a ver");
+        System.out.println("madmax8 vamo a ver otro aporte");
     }
     
 }
